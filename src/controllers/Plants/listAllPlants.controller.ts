9697ecf8304415bd3422eac9a48e7c8f56ce444c
Douglas <douglasclaudino543@gmail.com>
2024-04-04 02:26:25 -0300

    Refactor listAllPlants controller

--- src/controllers/Plants/listAllPlants.controller.ts
+++ src/controllers/Plants/listAllPlants.controller.ts
@@ -1,13 +1,13 @@
-import { Controller, Get } from "@nestjs/common";
-import { PrismaService } from "src/prisma/prisma-service";
-
+import { Controller, Get, HttpCode } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma-service';
 
 @Controller('/plants')
-export class listAllPlants{
+export class listAllPlants {
     constructor(private prisma: PrismaService) {}
-    
+
     @Get('/listAllPlants')
-    listplants(){
+    @HttpCode(200)
+    listplants() {
         return this.prisma.planta.findMany();
     }
 }
