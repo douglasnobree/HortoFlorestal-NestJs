b342bc9a7d88ab81c593451d5804ecb058db4a87
Douglas <douglasclaudino543@gmail.com>
2024-04-04 21:11:22 -0300

    Refactor deletePlants controller to use validation pipe and accept plant ID as a parameter

--- src/controllers/Plants/deletePlants.controller.ts
+++ src/controllers/Plants/deletePlants.controller.ts
@@ -3,20 +3,15 @@ import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 import { PrismaService } from "src/prisma/prisma-service";
 import { z } from "zod";
 
-const plantSchema = z.object({
-    id: z.number(),
-});
 
-type Plant = z.infer<typeof plantSchema>;
 
 
 @Controller('/plants')
 export class deletePlants{
     constructor(private prisma: PrismaService) {}
 
-    @Delete('/deletePlant/:id')
-    @UsePipes(new ZodValidationPipe(plantSchema))
-    deletePlants(@Param('id') id: number) {
+    @Delete('/deletePlant:id')
+    deletePlants(@Param('id') id: string) {
         console.log(id)
         console.log('Deletando planta')
         return this.prisma.planta.deleteMany();
