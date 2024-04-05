c4b0ba012013ab358f62592d694a185f27c1cdbb
Douglas <douglasclaudino543@gmail.com>
2024-04-04 21:30:21 -0300

    delete route complete

--- src/controllers/Plants/deletePlants.controller.ts
+++ src/controllers/Plants/deletePlants.controller.ts
@@ -1,19 +1,35 @@
-import { Controller, Delete, Param, UsePipes } from "@nestjs/common";
-import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
-import { PrismaService } from "src/prisma/prisma-service";
-import { z } from "zod";
-
-
-
+import {
+    Controller,
+    Delete,
+    HttpException,
+    Query,
+} from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma-service';
 
 @Controller('/plants')
-export class deletePlants{
+export class deletePlants {
     constructor(private prisma: PrismaService) {}
 
-    @Delete('/deletePlant:id')
-    deletePlants(@Param('id') id: string) {
-        console.log(id)
-        console.log('Deletando planta')
-        return this.prisma.planta.deleteMany();
+    @Delete('/deletePlant')
+    async deletePlants(@Query('id') id: string) {
+        const idNumber = parseInt(id);
+        console.log('Deletando planta com id:', id);
+        try {
+            await this.prisma.planta.delete({
+                where: {
+                    id: idNumber,
+                },
+            });
+            return { message: 'Planta deletada com sucesso' };
+        } catch (error) {
+            throw new HttpException(
+                {
+                    message: 'Plant not found',
+                    statuscode: 404,
+                    error: error,
+                },
+                404
+            );
+        }
     }
 }
